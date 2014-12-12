9b754157e91e2588ce95a8d27c3f249eac392cf4
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2014-12-12 12:40:15 -0600

    Clear gh-pages branch

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find . -name '.git' -prune -o \
-       -name '.gitignore' -prune -o \
-       -name '.xpi' -prune -o \
-       -name 'fw-update.sh' -prune -o \
-       -name 'build.sh' -prune -o \
-       -name '*~' -prune -o \
-       -print | xargs zip scaffold.xpi
